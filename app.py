c60ee484b1dda6ca15a7a1ca22b922aaa40492a1
williamgo05 <gomes.william18@yahoo.com.br>
2026-08-31 11:17:16 -0300

    edit hours and subtitle position

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+#Default
+
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+
+st.set_page_config(layout="wide")
 
 dados = pd.read_csv(
     "student_lifestyle_100k.csv",
@@ -9,6 +13,8 @@ dados = pd.read_csv(
 )
 
 dados = dados.drop(["Student_ID","Age","Department"], axis=1)
+
+#Default
 
 sem_depressão = dados[dados["Depression"] == False]
 com_depressão = dados[dados["Depression"] == True]
@@ -19,7 +25,7 @@ col1, col2 = st.columns(2)
 
 with col1:
 	st.subheader("Distribuição de estudantes por depressão")
-	fig, ax = plt.subplots(figsize=(8, 6))
+	fig, ax = plt.subplots(figsize=(10, 7))
 	total = quantidade.sum()
 
 	ax.pie(
@@ -35,9 +41,16 @@ media.index = ["Sem depressão", "Com depressão"]
 
 with col2:
 	st.subheader("Social Media x Depression")
-	fig, ax = plt.subplots(figsize=(8, 6))
+	fig, ax = plt.subplots(figsize=(10, 7))
 	media.plot.bar(ax=ax)
+	ax.bar_label(
+        ax.containers[0],
+        labels=[f"{valor:.2f} h" for valor in media],
+        padding=3
+    )
+
 	ax.set_title("Média de uso de redes sociais")
 	ax.set_ylabel("Horas")
+	ax.tick_params(axis="x", labelrotation=0)
 
 	st.pyplot(fig)
